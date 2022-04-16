6a7a306c9a6a29eabc8e1e6fadf227f50f56f3be
fabio-cumbo <fabio.cumbo@gmail.com>
2022-04-15 22:16:33 -0400

    Updating conda build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,9 @@
 mkdir -p $PREFIX/bin
 
 # Conda package requires chopin.py and functions.py scripts only
-# Everything else will be removed
 find . -type f \( -iname "chopin.py" -o -iname "functions.py" \) -exec cp {} $PREFIX/bin \;
+# Everything else can be removed
+rm -rf *
 
 # Make Python scripts executable
 chmod +x $PREFIX/bin/hdclass.py $PREFIX/bin/functions.py
