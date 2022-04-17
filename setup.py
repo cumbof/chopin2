5cc24de9ae7be75f97f74dea643907abd74dbdb7
fabio-cumbo <fabio.cumbo@gmail.com>
2022-04-16 21:08:09 -0400

    Updating name to chopin2

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import sys, setuptools
 
 if float("{}.{}".format(sys.version_info[0], sys.version_info[1])) < 3.8:
-    sys.stdout.write("chopin requires Python 3.8 or higher. Please update your Python installation")
+    sys.stdout.write("chopin2 requires Python 3.8 or higher. Please update your Python installation")
 
-setuptools.setup(name='chopin',
+setuptools.setup(name='chopin2',
                  version='1.0',
                  author='Fabio Cumbo',
                  author_email='fabio.cumbo@gmail.com',
-                 url='http://github.com/fabio-cumbo/chopin',
+                 url='http://github.com/fabio-cumbo/chopin2',
                  license='LICENSE',
                  packages=setuptools.find_packages(),
                  entry_points={
-                     'console_scripts': ['chopin = src.chopin:chopin']
+                     'console_scripts': ['chopin2 = src.chopin2:chopin2']
                  },
                  description='Supervised Classification with Hyperdimensional Computing',
                  long_description=open('README.md').read(),
