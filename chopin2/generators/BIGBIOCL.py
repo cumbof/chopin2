5aafea304245128a86a24864c9ff9d70a0f18169
fabio-cumbo <fabio.cumbo@gmail.com>
2022-04-21 12:47:44 -0400

    Updating BIGBIOCL and OpenGDC generators

--- chopin2/generators/BIGBIOCL.py
+++ chopin2/generators/BIGBIOCL.py
@@ -1,40 +1,25 @@
 import os, pickle, random
 
-features = [ ]
-trainData = [ ]
-trainLabels = [ ]
-testData = [ ]
-testLabels = [ ]
-
-# We just need the training percentage
-# the test percentage is 100.0 - training_perc
-training_perc = 80.0
-
 basepath = "/Users/fabio/GitHub/HD-Classifier/datasets/BIGBIOCL/thca_dnameth/"
 inputname = "thca_dnameth_NOCONTROL"
 inputfile = os.path.join( basepath, "{}.csv".format(inputname) )
 
-classes = { }
-with open( inputfile ) as dataset:
+features = list()
+classes = dict()
+with open(inputfile) as dataset:
     # skip header
-    features = dataset.readline().strip().split( ',' )[ 1: -1 ]
+    features = dataset.readline().strip().split(',')[1:-1]
     # read dataset
     for line in dataset:
         line = line.strip()
         if line:
-            line_split = line.split( ',' )
-            if line_split[ -1 ] not in classes:
-                classes[ line_split[ -1 ] ] = [ ]
-            classes[ line_split[ -1 ] ].append( line_split[ 0 ] )
+            line_split = line.split(',')
+            if line_split[-1] not in classes:
+                classes[line_split[-1]] = list()
+            classes[line_split[-1]].append(line_split[0])
 
-training_samples = [ ]
-test_samples = [ ]
-for label in classes:
-    training_amount = int( ( float( len( classes[ label ] ) ) * float( training_perc ) ) / 100.0 )
-    # Create the training set by random sampling
-    training_samples.extend( random.sample( classes[ label ], training_amount ) )
-    test_samples.extend( [ sample for sample in classes[ label ] if sample not in training_samples ] )
-
+allData = list()
+allLabels = list()
 with open( inputfile ) as dataset:
     # skip header
     dataset.readline()
@@ -42,28 +27,22 @@ with open( inputfile ) as dataset:
     for line in dataset:
         line = line.strip()
         if line:
-            line_split = line.split( ',' )
-            sample = line_split[ 0 ]
-            label = line_split[ -1 ]
-            line_split = line_split[ 1: -1 ]
-            for idx in range( len( line_split ) ):
-                if line_split[ idx ] == "?":
-                    line_split[ idx ] = 0.0
+            line_split = line.split(',')
+            sample = line_split[0]
+            label = line_split[-1]
+            line_split = line_split[1:-1]
+            for idx in range(len(line_split)):
+                if line_split[idx] == "?":
+                    line_split[idx] = 0.0
                 else:
-                    line_split[ idx ] = float( line_split[ idx ] )
-            if sample in training_samples:
-                trainData.append( line_split )
-                trainLabels.append( label )
-            elif sample in test_samples:
-                testData.append( line_split )
-                testLabels.append( label )
+                    line_split[idx] = float(line_split[idx])
+            allData.append(line_split)
+            allLabels.append(label)
 
-print( len( features ) )
-print( len( trainData ) )
-print( len( trainLabels ) )
-print( len( testData ) )
-print( len( testLabels ) )
+print("Features: {}".format(len(features)))
+print("Data: {}".format(len(allData)))
+print("Labels: {}".format(len(set(allLabels))))
 
-pickledata = ( features, trainData, trainLabels, testData, testLabels )
-with open( os.path.join( basepath, '{}.pkl'.format(inputname)), 'wb' ) as f:
-    pickle.dump( pickledata, f )
+pickledata = (features, allData, allLabels)
+with open(os.path.join(basepath, '{}.pkl'.format(inputname)), 'wb') as f:
+    pickle.dump(pickledata, f)
--- chopin2/generators/OpenGDC.py
+++ chopin2/generators/OpenGDC.py
@@ -1,18 +1,8 @@
 import sys, os, pickle, random
 from pathlib import Path
 
-features = [ ]
-trainData = [ ]
-trainLabels = [ ]
-testData = [ ]
-testLabels = [ ]
-
-# We just need the training percentage
-# The test percentage is 100.0 - training_perc
-training_perc = 80.0
-
 program = 'tcga'
-tumor = '{}-acc'.format( program )
+tumor = '{}-acc'.format(program)
 
 # valueidx is the id of the column which contains the observation value for a specific experiment
 # It starts from 0
@@ -30,78 +20,65 @@ featureidx = 4
 
 # Input files have the same number of features, all in the same order
 basepath = '/Users/fabio/GitHub/HD-Classifier/datasets/OpenGDC'
-inputname = '{}__{}'.format( tumor, datatype )
+inputname = '{}__{}'.format(tumor, datatype)
 
-bed_basepath = '{}/{}/{}/{}/'.format( basepath, program, tumor, datatype )
-metadata_basepath = '{}/{}/{}/clinical_and_biospecimen_supplements/'.format( basepath, program, tumor )
+bed_basepath = '{}/{}/{}/{}/'.format(basepath, program, tumor, datatype)
+metadata_basepath = '{}/{}/{}/clinical_and_biospecimen_supplements/'.format(basepath, program, tumor)
 
-classes = { }
-bed_generator = Path( bed_basepath ).glob( '*.bed' )
+classes = dict()
+bed_generator = Path(bed_basepath).glob('*.bed')
 for filepath in bed_generator:
-    sample = '-'.join( os.path.splitext( os.path.basename( str( filepath ) ) )[ 0 ].split( '-' )[ 0: -1 ] )
-    metadata_filepath = os.path.join( metadata_basepath, '{}-{}.bed.meta'.format( sample, metaabbr ) )
+    sample = '-'.join(os.path.splitext(os.path.basename(str(filepath)))[0].split('-')[0:-1])
+    metadata_filepath = os.path.join(metadata_basepath, '{}-{}.bed.meta'.format(sample, metaabbr))
     platform = None
     label = None
-    if os.path.exists( metadata_filepath ):
-        with open( metadata_filepath ) as metadata:
+    if os.path.exists(metadata_filepath):
+        with open(metadata_filepath) as metadata:
             for line in metadata:
                 line = line.strip()
                 if line:
-                    line_split = line.split( '\t' )
-                    if line_split[ 0 ] == 'manually_curated__tissue_status':
-                        label = line_split[ 1 ]
-                    elif line_split[ 0 ] == 'gdc__platform':
-                        platform = line_split[ 1 ]
+                    line_split = line.split('\t')
+                    if line_split[0] == 'manually_curated__tissue_status':
+                        label = line_split[1]
+                    elif line_split[0] == 'gdc__platform':
+                        platform = line_split[1]
     if ( datatype == 'methylation_beta_value' and platform == 'Illumina Human Methylation 450' ) or \
             not datatype == 'methylation_beta_value' and label:
         if label not in classes:
-            classes[ label ] = [ ]
-        classes[ label ].append( sample )
+            classes[label] = list()
+        classes[label].append(sample)
 
-# Start processing if more than one class has been detected
-if len( classes ) > 1:
-    training_samples = [ ]
-    test_samples = [ ]
+features = list()
+allData = list()
+allFeatures = list()
+
+bed_generator = Path( bed_basepath ).glob('*.bed')
+for filepath in bed_generator:
+    sample = '-'.join(os.path.splitext(os.path.basename(str(filepath)))[0].split('-')[0:-1])
+    print('Reading {}'.format(sample))
+    values = list()
+    addFeatures = len(features) == 0
+    with open(str(filepath)) as samplefile:
+        for line in samplefile:
+            line = line.strip()
+            if line:
+                value = line.split('\t')[valueidx]
+                if value.strip():
+                    values.append(float(value))
+                else:
+                    values.append(0.0)
+                if addFeatures:
+                    features.append(line.split('\t')[featureidx])
+    allData.append(values)
     for label in classes:
-        print( '{}: {}'.format( label, len( classes[ label ] ) ) )
-        training_amount = int( ( float( len( classes[ label ] ) ) * float( training_perc ) ) / 100.0 )
-        # Create the training set by random sampling
-        training_samples.extend( random.sample( classes[ label ], training_amount ) )
-        test_samples.extend( [ sample for sample in classes[ label ] if sample not in training_samples ] )
+        if sample in classes[label]:
+            allLabels.append(label)
+            break
 
-    bed_generator = Path( bed_basepath ).glob( '*.bed' )
-    for filepath in bed_generator:
-        sample = '-'.join( os.path.splitext( os.path.basename( str( filepath ) ) )[ 0 ].split( '-' )[ 0: -1 ] )
-        if sample in training_samples or sample in test_samples:
-            print( 'Reading {}'.format( sample ) )
-            values = [ ]
-            addFeatures = len( features ) == 0
-            with open( str( filepath ) ) as samplefile:
-                for line in samplefile:
-                    line = line.strip()
-                    if line:
-                        value = line.split( '\t' )[ valueidx ]
-                        if value.strip():
-                            values.append( float( value ) )
-                        else:
-                            values.append( 0.0 )
-                        if addFeatures:
-                            features.append( line.split( '\t' )[ featureidx ] )
-            trainData.append( values ) if sample in training_samples else testData.append( values )
-            for label in classes:
-                if sample in classes[ label ]:
-                    trainLabels.append( label ) if sample in training_samples else testLabels.append( label )
-                    break
+print("Features: {}".format(len(features)))
+print("Data: {}".format(len(allData)))
+print("Labels: {}".format(len(set(allLabels))))
 
-    print( len( features ) )
-    print( len( trainData ) )
-    print( len( trainLabels ) )
-    print( len( testData ) )
-    print( len( testLabels ) )
-
-    pickledata = ( features, trainData, trainLabels, testData, testLabels )
-    with open( os.path.join( basepath, '{}.pkl'.format(inputname)), 'wb' ) as f:
-        pickle.dump( pickledata, f )
-else:
-    print( '{} classes found'.format( len( classes ) ) )
-    sys.exit( 1 )
+pickledata = (features, allData, allLabels)
+with open(os.path.join(basepath, '{}.pkl'.format(inputname)), 'wb') as f:
+    pickle.dump(pickledata, f)
