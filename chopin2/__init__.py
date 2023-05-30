55a01ac51232a888e7912aca393bb3eb660ed4d6
fabio-cumbo <fabio.cumbo@gmail.com>
2023-05-30 17:12:01 -0400

    Update __init__.py
    Add __authors__, __version__, and __date__

---
+++ chopin2/__init__.py
@@ -0,0 +1,5 @@
+__authors__ = ("Fabio Cumbo (fabio.cumbo@gmail.com)",
+               "Simone Truglia (s.truglia@students.uninettunouniversity.net)")
+
+__version__ = "1.0.7"
+__date__ = "Apr 21, 2022"
