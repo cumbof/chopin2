b44ebd113128d3c37a588cd29e0902c16051bceb
Fabio Cumbo <fabio.cumbo@gmail.com>
2024-06-14 16:46:50 -0400

    Bump chopin2 version to v1.0.9.post1

--- chopin2/__init__.py
+++ chopin2/__init__.py
@@ -1,5 +1,5 @@
 __authors__ = ("Fabio Cumbo (fabio.cumbo@gmail.com)",
                "Simone Truglia (s.truglia@students.uninettunouniversity.net)")
 
-__version__ = "1.0.9"
+__version__ = "1.0.9.post1"
 __date__ = "Jun 14, 2024"
